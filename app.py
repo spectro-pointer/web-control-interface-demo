77ca7f4d70a88667f012cef3436eacb957b4b304
sasha42 <sasha42@users.noreply.github.com>
2016-05-07 19:48:33 +0200

    Added base functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import xmlrpclib
 
 app = Flask(__name__)
@@ -6,10 +6,22 @@ app = Flask(__name__)
 server_url = 'http://192.168.0.101:8000/';
 server = xmlrpclib.Server(server_url);
 
-@app.route('/secreturl')
+@app.route('/secreturl', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        if request.form['submit'] == 'Do Something':
+            result = server.set_motores(4, "Busca")
+            return render_template('control-interface.html')
+            pass # do something
+        elif request.form['submit'] == 'Do Something Else':
+            result = server.set_motores(0, "Busca")
+            return render_template('control-interface.html')
+            pass # do something else
+        else:
+            pass # unknown
+    elif request.method == 'GET':
     #result = server.set_motores(4, Busca)
-    return render_template('control-interface.html')
+        return render_template('control-interface.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000, debug=True)
