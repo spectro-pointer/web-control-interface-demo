5ed3dfc266fce6244e8cc907cba2824b58c5f870
sasha42 <sasha42@users.noreply.github.com>
2016-05-07 20:36:39 +0200

    Cleaning up app.py

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ import xmlrpclib
 
 app = Flask(__name__)
 
+# Enter your server URL below
 server_url = 'http://192.168.0.101:8000/';
 server = xmlrpclib.Server(server_url);
 
+# Main logic tree for controller
 @app.route('/secreturl', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -21,5 +23,6 @@ def index():
             result = server.set_motores(0, "Busca")
     return render_template('control-interface.html')  
 
+# Run the web app
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8000)
