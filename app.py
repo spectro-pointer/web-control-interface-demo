5b3bd14f274241410fc1a40cda65513602067913
sasha42 <sasha42@users.noreply.github.com>
2016-05-07 19:54:33 +0200

    Basic controls working

--- app.py
+++ app.py
@@ -9,19 +9,17 @@ server = xmlrpclib.Server(server_url);
 @app.route('/secreturl', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        if request.form['submit'] == 'Do Something':
+        if request.form['submit'] == 'Left':
+            result = server.set_motores(1, "Busca")
+        elif request.form['submit'] == 'Right':
+            result = server.set_motores(3, "Busca")
+        elif request.form['submit'] == 'Down':
             result = server.set_motores(4, "Busca")
-            return render_template('control-interface.html')
-            pass # do something
-        elif request.form['submit'] == 'Do Something Else':
+        elif request.form['submit'] == 'Up':
+            result = server.set_motores(12, "Busca")
+        elif request.form['submit'] == 'Stop':
             result = server.set_motores(0, "Busca")
-            return render_template('control-interface.html')
-            pass # do something else
-        else:
-            pass # unknown
-    elif request.method == 'GET':
-    #result = server.set_motores(4, Busca)
-        return render_template('control-interface.html')
+    return render_template('control-interface.html')  
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000, debug=True)
